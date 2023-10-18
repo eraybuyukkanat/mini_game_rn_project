12dc1d19b25815c54e03a81703f5e6f6cefc92a2
eraybuyukkanat <erybykknt@gmail.com>
2023-10-18 13:27:28 +0300

    improved

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,7 @@ import { StyleSheet } from "react-native";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none" autoCorrect={false}/>
       <PrimaryButton>RESET</PrimaryButton>
       <PrimaryButton>CONFIRM</PrimaryButton>
     </View>
@@ -22,6 +22,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#72063c',
     borderRadius: 8,
     elevation: 10,
-   
+    alignItems: "center"
   },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center"
+  }
 });
